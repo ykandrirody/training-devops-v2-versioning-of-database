5826c7e17675d01e7dd5e7ca9046e833f83e70e5
ykandrirody <yassinek@gmail.com>
2016-05-28 03:05:51 +0200

    fix travis

--- run_practical.sh
+++ run_practical.sh
@@ -1,23 +1,23 @@
 echo 
 echo r_dev_v1
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v1.properties --contexts=init,prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v1.properties update'
 
 echo 
 echo r_dev_v2
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v2.properties --contexts=init,prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v2.properties update'
 
 echo 
 echo r_dev_v3
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v3.properties --contexts=init,prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_dev_v3.properties update'
 
 echo 
 echo r_prd_v1
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v1.properties --contexts=prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v1.properties update'
 
 echo 
 echo r_prd_v2
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v2.properties --contexts=prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v2.properties update'
 
 echo 
 echo r_prd_v3
-docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v3.properties --contexts=prod update'
+docker exec -it liquibase.global bash -c 'java -jar liquibase-3.4.2.jar --defaultsFile=r_prd_v3.properties update'
