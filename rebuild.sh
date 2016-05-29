0df7cfddc3658f80c9b1d839b37e5cd10c46d84f
ykandrirody <yassinek@gmail.com>
2016-05-29 02:13:46 +0200

    Improvement

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,6 @@
 docker-compose -f ./training-devops-v2-versioning-of-database/docker-compose.yml kill
 docker-compose -f ./training-devops-v2-versioning-of-database/docker-compose.yml rm -f -v -a
 
-#docker-compose -f ./training-devops-v2-versioning-of-database/docker-compose.yml pull
 docker-compose -f ./training-devops-v2-versioning-of-database/docker-compose.yml build
 
 docker-compose -f ./training-devops-v2-versioning-of-database/docker-compose.yml up -d
